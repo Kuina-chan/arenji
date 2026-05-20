fe8a89c6ef91abe580820e0ba51d65a457daa803
Kuina <75318520+Kuina-chan@users.noreply.github.com>
2026-05-20 10:53:18 +0700

    This Audio Engine will be useful later when vst support is online

---
+++ arenji/arenji.Game/arenjiAudioEngine.cs
@@ -0,0 +1,21 @@
+using osu.Framework.Timing;
+
+namespace arenji.Game
+{
+    public interface IArenjiAudioEngine
+    {
+        // The visualizer will read this clock to move the notes
+        IClock AudioClock { get; }
+        
+        // Information about the track
+        double DurationMs { get; }
+        bool IsReady { get; }
+
+        // The core commands
+        void LoadFiles(string midiPath, string instrumentPath);
+        void Play();
+        void Pause();
+        void Seek(double timeMs);
+        void Dispose();
+    }
+}
