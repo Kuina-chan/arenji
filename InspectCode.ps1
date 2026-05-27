6f38240f2ea496f8323e815bf31a1049e9340359
Kuina <75318520+Kuina-chan@users.noreply.github.com>
2026-05-27 19:51:26 +0700

    Undo the whole osu!framework source code. Credit to Nutch on discord for this change lmao

--- InspectCode.ps1
+++
@@ -1,7 +0,0 @@
-dotnet tool restore
-
-dotnet CodeFileSanity
-dotnet jb inspectcode "osu-framework.Desktop.slnf" --no-build --output="inspectcodereport.xml" --caches-home="inspectcode" --verbosity=WARN
-dotnet nvika parsereport "inspectcodereport.xml" --treatwarningsaserrors
-
-exit $LASTEXITCODE
--- osu.Framework/Resources/Shaders/Internal/sh_Compatibility.h
+++
@@ -1,9 +0,0 @@
-// This file is automatically included in every shader.
-
-#ifndef INTERNAL_COMPATIBILITY_H
-#define INTERNAL_COMPATIBILITY_H
-
-#version 450
-#extension GL_ARB_uniform_buffer_object : enable
-
-#endif
